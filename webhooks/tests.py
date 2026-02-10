004ccba8f0334a2563defb66f9a7fdd722bf9cb4
Ursu Tudor <tudor_ursu90@yahoo.com>
2026-02-10 21:54:33 +0200

    Add receiver view skeleton and tests
    The view receives a idempotency key, checks if an event for that
    already exists and returns or creates one and returns if not.

--- webhooks/tests.py
+++ webhooks/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from .models import Event
 from django.db.utils import IntegrityError
 from django.core.exceptions import ValidationError
+from django.urls import reverse
 
 def create_event(idempotency_key):
   return Event.objects.create(idempotency_key=idempotency_key)
@@ -26,3 +27,25 @@ class EventModelTests(TestCase):
     e = Event.objects.create(idempotency_key = "x" * 256)
     with self.assertRaises(ValidationError):
         e.full_clean()
+
+class ReceiverViewTest(TestCase):
+  def test_returns_OK_if_event_already_exists(self):
+    Event.objects.create(idempotency_key="abc")
+    url = reverse("webhooks:receiver")
+    data = { "idempotency_key": "abc" }
+    response = self.client.post(url, data, content_type="application/json")
+    self.assertEqual(response.status_code, 200)
+
+  def test_creates_a_new_event(self):
+    url = reverse("webhooks:receiver")
+    data = { "idempotency_key": "456" }
+    response = self.client.post(url, data, content_type="application/json")
+
+    self.assertIs(Event.objects.filter(idempotency_key="456").exists(), True)
+    self.assertEqual(response.status_code, 200)
+
+  def test_fails_without_idempotency_key(self):
+    url = reverse("webhooks:receiver")
+    data = { "idempotency_key": None }
+    response = self.client.post(url, data, content_type="application/json")
+    self.assertEqual(response.status_code, 400)
--- webhooks/views.py
+++ webhooks/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Event
+import json
 
-# Create your views here.
+def receiver(request):
+  if request.method != "POST": 
+    return HttpResponse(status=405) 
+
+  data = json.loads(request.body) 
+  idempotency_key = data.get("idempotency_key") 
+
+  if not idempotency_key:
+    return HttpResponse("Missing idempotency_key", status=400)
+  
+  if Event.objects.filter(idempotency_key=idempotency_key).exists(): 
+    return HttpResponse(status=200) 
+
+  Event.objects.create(idempotency_key=idempotency_key)
+  
+  return HttpResponse(status=200)
+
