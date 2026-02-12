8811422ee8e69acb5abc5db0dec4253fbeee9f6a
Ursu Tudor <tudor_ursu90@yahoo.com>
2026-02-12 21:56:15 +0200

    Update env to python 3.12 and add signature validation
    I've stored a made-up secret in a .env file that I load into settings
    and import into my view and the view tests.
    Both the tests and the view use hmac to create a signature from the secret
    and the raw body of the request.
    The tests then send said signature in the header, like a real sender would,
    while the view picks it up, reconstructs it using its own stored secret
    and the raw body, and compares the two using hmac's compare_digest method
    to avoid timing attacks.
    
    The _verify_signature method returns the result of this comparison. If
    False, the receiver view return a 401, if true, it moves on to the steps
    built previously: checking the idempotency_key and creating a new event
    or simply returning 200 if one with that key already exists.

--- webhooks/tests.py
+++ webhooks/tests.py
@@ -4,6 +4,8 @@ from .models import Event
 from django.db.utils import IntegrityError
 from django.core.exceptions import ValidationError
 from django.urls import reverse
+import json, hmac, hashlib
+from django.conf import settings
 
 def create_event(idempotency_key):
   return Event.objects.create(idempotency_key=idempotency_key)
@@ -29,17 +31,80 @@ class EventModelTests(TestCase):
         e.full_clean()
 
 class ReceiverViewTest(TestCase):
+  def test_returns_OK_with_signature(self):
+    url = reverse("webhooks:receiver")
+    data = { "idempotency_key" : "123"}
+
+    bytes_secret = json.dumps(settings.WEBHOOK_SECRET).encode("utf-8")
+    payload_bytes = json.dumps(data).encode("utf-8")
+    signature = hmac.new(bytes_secret, payload_bytes, hashlib.sha256).hexdigest()
+
+    response = self.client.post(
+      url, 
+      data, 
+      content_type="application/json",  
+      HTTP_EXAMPLE_SIGNATURE=signature
+    )
+    self.assertEqual(response.status_code, 200)
+
+  def test_returns_401_with_wrong_signature(self):
+    url = reverse("webhooks:receiver")
+    data = { "idempotency_key" : "123"}
+
+    bytes_secret = json.dumps("fdsahkjt674t1dfgasf").encode("utf-8")
+    payload_bytes = json.dumps(data).encode("utf-8")
+    signature = hmac.new(bytes_secret, payload_bytes, hashlib.sha256).hexdigest()
+
+    response = self.client.post(
+      url, 
+      data, 
+      content_type="application/json",  
+      HTTP_EXAMPLE_SIGNATURE=signature
+    )
+    self.assertEqual(response.status_code, 401)
+
+  def test_returns_401_with_missing_signature(self):
+    url = reverse("webhooks:receiver")
+    data = { "idempotency_key" : "123"}
+
+    response = self.client.post(
+      url, 
+      data, 
+      content_type="application/json",  
+    )
+    self.assertEqual(response.status_code, 401)
+
   def test_returns_OK_if_event_already_exists(self):
     Event.objects.create(idempotency_key="abc")
     url = reverse("webhooks:receiver")
     data = { "idempotency_key": "abc" }
-    response = self.client.post(url, data, content_type="application/json")
+
+    bytes_secret = json.dumps(settings.WEBHOOK_SECRET).encode("utf-8")
+    payload_bytes = json.dumps(data).encode("utf-8")
+    signature = hmac.new(bytes_secret, payload_bytes, hashlib.sha256).hexdigest()
+
+    response = self.client.post(
+      url, 
+      data, 
+      content_type="application/json",  
+      HTTP_EXAMPLE_SIGNATURE=signature
+    )
     self.assertEqual(response.status_code, 200)
 
   def test_creates_a_new_event(self):
     url = reverse("webhooks:receiver")
     data = { "idempotency_key": "456" }
-    response = self.client.post(url, data, content_type="application/json")
+
+    bytes_secret = json.dumps(settings.WEBHOOK_SECRET).encode("utf-8")
+    payload_bytes = json.dumps(data).encode("utf-8")
+    signature = hmac.new(bytes_secret, payload_bytes, hashlib.sha256).hexdigest()
+
+    response = self.client.post(
+      url, 
+      data, 
+      content_type="application/json",  
+      HTTP_EXAMPLE_SIGNATURE=signature
+    )
 
     self.assertIs(Event.objects.filter(idempotency_key="456").exists(), True)
     self.assertEqual(response.status_code, 200)
@@ -47,5 +112,15 @@ class ReceiverViewTest(TestCase):
   def test_fails_without_idempotency_key(self):
     url = reverse("webhooks:receiver")
     data = { "idempotency_key": None }
-    response = self.client.post(url, data, content_type="application/json")
+    
+    bytes_secret = json.dumps(settings.WEBHOOK_SECRET).encode("utf-8")
+    payload_bytes = json.dumps(data).encode("utf-8")
+    signature = hmac.new(bytes_secret, payload_bytes, hashlib.sha256).hexdigest()
+
+    response = self.client.post(
+      url, 
+      data, 
+      content_type="application/json",  
+      HTTP_EXAMPLE_SIGNATURE=signature
+    )
     self.assertEqual(response.status_code, 400)
--- webhooks/views.py
+++ webhooks/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Event
-import json
+import json, hmac, hashlib
+from django.conf import settings
+
+def _verify_signature(request):
+  signature = request.headers.get("Example-Signature")
+
+  if not signature: return False
+
+  body = request.body
+  bytes_secret = json.dumps(settings.WEBHOOK_SECRET).encode("utf-8")
+
+  expected_signature = hmac.new(bytes_secret, body, hashlib.sha256).hexdigest()
+  return hmac.compare_digest(signature, expected_signature)
 
 def receiver(request):
   if request.method != "POST": 
     return HttpResponse(status=405) 
+
+  if not _verify_signature(request): 
+    return HttpResponse("Wrong signature", status=401)
 
   data = json.loads(request.body) 
   idempotency_key = data.get("idempotency_key") 
